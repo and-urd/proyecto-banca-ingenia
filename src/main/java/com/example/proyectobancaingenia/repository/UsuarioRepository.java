e8f9250eb0b5b829f7416cffbd4a961ae24a6e41
andresurda@gmail.com <andresurda@gmail.com>
2021-05-13 09:12:09 +0200

    Elimino clase Usuario, su repositorio, service y Controller.

--- src/main/java/com/example/proyectobancaingenia/repository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.proyectobancaingenia.repository;
-
-import com.example.proyectobancaingenia.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-}
