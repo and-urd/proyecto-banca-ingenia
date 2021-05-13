92e08ed1ea26f1d84b93b8f9916a60de946e5659
roriol <rafael.oriol.pedrosa@gmail.com>
2021-05-13 08:07:28 +0200

    Clases vacias borradas, Clase Cuenta.java empezada y application.properties modificado

--- src/main/java/com/example/proyectobancaingenia/modelbanca/Cuenta.java
+++ src/main/java/com/example/proyectobancaingenia/modelbanca/Cuenta.java
@@ -1,4 +1,24 @@
 package com.example.proyectobancaingenia.modelbanca;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="cuentas")
 public class Cuenta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="numero_cuenta")
+    private String numeroCuenta;
+
+    @Column(name="tipo_cuenta")
+    private String tipoCuenta;
+
+    @Column(name="saldo")
+    private Double saldo;
+
+
+
 }
