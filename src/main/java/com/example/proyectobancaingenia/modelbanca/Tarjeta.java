da176b6baa7f82b5cfb40c90a23b224a20593551
andresurda@gmail.com <andresurda@gmail.com>
2021-05-13 10:03:59 +0200

    Creo modelos Movimiento y Tarjeta

--- src/main/java/com/example/proyectobancaingenia/modelbanca/Tarjeta.java
+++ src/main/java/com/example/proyectobancaingenia/modelbanca/Tarjeta.java
@@ -1,4 +1,112 @@
 package com.example.proyectobancaingenia.modelbanca;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name="tarjetas")
 public class Tarjeta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="numero_tarjeta")
+    private String numeroTarjeta;
+
+    @Column(name="fecha_caducidad")
+    private LocalDate fechaCaducidad;
+
+    private Integer cvv;
+
+    @Column(name="limite_maximo")
+    private Double limiteMaximo;
+
+    @Column(name = "tipo_tarjeta")
+    private String tipoTarjeta;
+
+    // todo - FK Cuenta (ManyToOne)
+
+    // CONSTRUCTORES
+
+    public Tarjeta() {
+    }
+
+    public Tarjeta(String numeroTarjeta, LocalDate fechaCaducidad, Integer cvv, Double limiteMaximo, String tipoTarjeta) {
+        this.numeroTarjeta = numeroTarjeta;
+        this.fechaCaducidad = fechaCaducidad;
+        this.cvv = cvv;
+        this.limiteMaximo = limiteMaximo;
+        this.tipoTarjeta = tipoTarjeta;
+
+        // TODO - faltan campos FK
+    }
+
+    // GETTERS Y SETTERS
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNumeroTarjeta() {
+        return numeroTarjeta;
+    }
+
+    public void setNumeroTarjeta(String numeroTarjeta) {
+        this.numeroTarjeta = numeroTarjeta;
+    }
+
+    public LocalDate getFechaCaducidad() {
+        return fechaCaducidad;
+    }
+
+    public void setFechaCaducidad(LocalDate fechaCaducidad) {
+        this.fechaCaducidad = fechaCaducidad;
+    }
+
+    public Integer getCvv() {
+        return cvv;
+    }
+
+    public void setCvv(Integer cvv) {
+        this.cvv = cvv;
+    }
+
+    public Double getLimiteMaximo() {
+        return limiteMaximo;
+    }
+
+    public void setLimiteMaximo(Double limiteMaximo) {
+        this.limiteMaximo = limiteMaximo;
+    }
+
+    public String getTipoTarjeta() {
+        return tipoTarjeta;
+    }
+
+    public void setTipoTarjeta(String tipoTarjeta) {
+        this.tipoTarjeta = tipoTarjeta;
+    }
+
+    // TODO - faltan campos FK
+
+    // METODO TOSTRING
+
+    @Override
+    public String toString() {
+        return "Tarjeta{" +
+                "id=" + id +
+                ", numeroTarjeta='" + numeroTarjeta + '\'' +
+                ", fechaCaducidad=" + fechaCaducidad +
+                ", cvv=" + cvv +
+                ", limiteMaximo=" + limiteMaximo +
+                ", tipoTarjeta='" + tipoTarjeta + '\'' +
+                '}';
+
+        // TODO - faltan campos FK
+    }
 }
