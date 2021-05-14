979798971f496192266daf7afd67cf465c83942c
roriol <rafael.oriol.pedrosa@gmail.com>
2021-05-14 13:13:41 +0200

    Main app renamed

--- src/main/java/com/example/proyectobancaingenia/EjercicioHibernateAndresApplication.java
+++ src/main/java/com/example/proyectobancaingenia/EjercicioHibernateAndresApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProyectoBancaIngeniaApplication {
+public class EjercicioHibernateAndresApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProyectoBancaIngeniaApplication.class, args);
+		SpringApplication.run(EjercicioHibernateAndresApplication.class, args);
 	}
 
 }
